191781c3f5e7bbe3a5b28bf00f4b790816d188ab
Yossi's macbook air <bentzi18@gmail.com>
2017-06-18 20:34:11 +1000

    regular

--- First App/ViewController.swift
+++ First App/ViewController.swift
@@ -3,7 +3,7 @@
 //  First App
 //
 //  Created by Yossi's macbook air on 7/6/17.
-//  Copyright © 2017 Yossi's macbook air. All rights reserved...uuytuytuy
+//  Copyright © 2017 Yossi's macbook air. All rights reserved.
 //
 
 import UIKit
